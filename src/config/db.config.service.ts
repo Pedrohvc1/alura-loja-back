e641c99c9ee360989f00df29f70d41f5741e4e40
pedro.vieira <pedro.vieira@ferreiracosta.com.br>
2024-03-18 15:15:16 -0300

    feat: add Entity,   Column,   CreateDateColumn,   UpdateDateColumn,   DeleteDateColumn,   PrimaryGeneratedColumn

--- src/config/db.config.service.ts
+++ src/config/db.config.service.ts
@@ -14,7 +14,7 @@ export class DbConfigService implements TypeOrmOptionsFactory {
       username: this.configService.get<string>('DB_USERNAME'),
       password: this.configService.get<string>('DB_PASSWORD'),
       database: this.configService.get<string>('DB_NAME'),
-      entities: ['dist/**/*.entity{.ts,.js}'],
+      entities: [__dirname + '/**/*.entity{.ts,.js}'], // caminho para as entidades
       synchronize: true,
     };
   }
