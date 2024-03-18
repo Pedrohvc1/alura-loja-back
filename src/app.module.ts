0df6992b88bed3dcb477a0936f289a1c313ff473
pedro.vieira <pedro.vieira@ferreiracosta.com.br>
2024-03-18 12:42:25 -0300

    feat: add postgresql

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,7 +1,22 @@
 import { Module } from '@nestjs/common';
 import { UsuarioModule } from './usuario/usuario.module';
+import { DbConfigService } from './config/db.config.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
-  imports: [UsuarioModule],
+  imports: [
+    UsuarioModule,
+    // ProdutoModule,
+
+    ConfigModule.forRoot({
+      isGlobal: true,
+    }), // faz com que o módulo seja global, através da lib @nestjs/config
+
+    TypeOrmModule.forRootAsync({
+      useClass: DbConfigService,
+      inject: [DbConfigService],
+    }), // add o config do banco de dados
+  ],
 })
 export class AppModule {}
